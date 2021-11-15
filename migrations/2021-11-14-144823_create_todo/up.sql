0cbea29d31c6bb615eb14f804b0306e8647c8924
nurakmaljalil91 <nurakmaljalil91@gmail.com>
2021-11-15 22:07:53 +0800

    Establish Connection to Postgresql

--- migrations/2021-11-14-144823_create_todo/up.sql
+++ migrations/2021-11-14-144823_create_todo/up.sql
@@ -1,8 +1,7 @@
 -- Your SQL goes here
-CREATE TABLE posts
+CREATE TABLE todo
 (
-    id        SERIAL PRIMARY KEY,
-    title     VARCHAR NOT NULL,
-    body      TEXT    NOT NULL,
-    published BOOLEAN NOT NULL DEFAULT 'f'
+    id       SERIAL PRIMARY KEY,
+    name     VARCHAR NOT NULL,
+    complete BOOLEAN NOT NULL DEFAULT 'f'
 )
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,51 @@
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate dotenv;
+
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use dotenv::dotenv;
+use std::env;
+// use rocket::tokio::time::{sleep, Duration};
+//
+// mod schema;
+//
+//
+// fn hello_world(){
+//     println!("Hello, world!");
+// }
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "Hello World"
+}
+
+// #[get("/delay/<seconds>")]
+// async fn delay(seconds: u64) -> String {
+//     sleep(Duration::from_secs(seconds)).await;
+//     format!("Waited for {} seconds", seconds)
+// }
+//
+// #[rocket::main]
+// async fn main() {
+//     hello_world();
+//     rocket::build()
+//         .mount("/", routes![delay])
+//         .launch().await;
+// }
+
+pub fn establish_connection() -> PgConnection{
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+
+    PgConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .mount("/", routes![index])
+    establish_connection();
+    rocket::build().mount("/", routes![index])
 }
